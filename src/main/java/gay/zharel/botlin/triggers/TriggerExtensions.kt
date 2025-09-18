b6f83ee97ff264495381384482131233cd0fdf63
Tim Winters <tim.winters@bevi.co>
2025-09-18 14:35:26 -0400

    Expand trigger logic

--- src/main/java/gay/zharel/botlin/triggers/TriggerExtensions.kt
+++ src/main/java/gay/zharel/botlin/triggers/TriggerExtensions.kt
@@ -1,39 +1,33 @@
 package gay.zharel.botlin.triggers
 
-import java.util.function.BooleanSupplier
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import java.util.function.BooleanSupplier
 
-/**
- * Boolean NOT
- */
+/** Boolean NOT */
 operator fun Trigger.not(): Trigger = this.negate()
 
-/**
- * Infix boolean AND
- */
+/** Infix boolean AND */
 infix fun Trigger.and(trigger: BooleanSupplier): Trigger = this.and(trigger)
 
-/**
- * Infix boolean NAND
- */
-infix fun Trigger.nand(trigger: BooleanSupplier): Trigger = this.and(trigger).negate()
+/** Infix boolean NAND */
+infix fun Trigger.nand(trigger: BooleanSupplier): Trigger = Trigger {
+    !(asBoolean && trigger.asBoolean)
+}
 
-/**
- * Infix boolean OR
- */
+/** Infix boolean OR */
 infix fun Trigger.or(trigger: BooleanSupplier): Trigger = this.or(trigger)
 
-/**
- * Infix boolean NOR
- */
-infix fun Trigger.nor(trigger: BooleanSupplier): Trigger = this.or(trigger).negate()
+/** Infix boolean NOR */
+infix fun Trigger.nor(trigger: BooleanSupplier): Trigger = Trigger {
+    !(asBoolean || trigger.asBoolean)
+}
 
-/**
- * Infix boolean XOR
- */
-infix fun Trigger.xor(trigger: BooleanSupplier): Trigger = this.or(trigger).and(this.and(trigger).negate())
+/** Infix boolean XOR */
+infix fun Trigger.xor(trigger: BooleanSupplier): Trigger = Trigger {
+    asBoolean xor trigger.asBoolean
+}
 
-/**
- * Infix boolean XNOR
- */
-infix fun Trigger.xnor(trigger: BooleanSupplier): Trigger = this.and(trigger).or(this.or(trigger).negate())
+/** Infix boolean XNOR */
+infix fun Trigger.xnor(trigger: BooleanSupplier): Trigger = Trigger {
+    (asBoolean && trigger.asBoolean) || !(asBoolean || trigger.asBoolean)
+}

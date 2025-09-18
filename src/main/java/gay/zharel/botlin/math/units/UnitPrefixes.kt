7edfb33154fe4276afc7770a3d8203a127734306
Zach Harel <zach@zharel.me>
2025-09-18 15:05:57 -0400

    Refactor unit prefixes to use constants for improved performance and readability

--- src/main/java/gay/zharel/botlin/math/units/UnitPrefixes.kt
+++ src/main/java/gay/zharel/botlin/math/units/UnitPrefixes.kt
@@ -5,32 +5,32 @@ package gay.zharel.botlin.math.units
  */
 
 object Prefixes {
-    inline val quetta: Double get() = 1E30
-    inline val ronna: Double get() = 1E27
-    inline val yotta: Double get() = 1E24
-    inline val zetta: Double get() = 1E21
-    inline val exa: Double get() = 1E18
-    inline val peta: Double get() = 1E15
-    inline val tera: Double get() = 1E12
-    inline val giga: Double get() = 1E9
-    inline val mega: Double get() = 1E6
-    inline val kilo: Double get() = 1E3
-    inline val hecto: Double get() = 1E2
-    inline val deca: Double get() = 1E1
-    inline val deci: Double get() = 1E-1
-    inline val centi: Double get() = 1E-2
-    inline val milli: Double get() = 1E-3
-    inline val micro: Double get() = 1E-6
-    inline val nano: Double get() = 1E-9
-    inline val pico: Double get() = 1E-12
-    inline val femto: Double get() = 1E-15
-    inline val atto: Double get() = 1E-18
-    inline val zepto: Double get() = 1E-21
-    inline val yocto: Double get() = 1E-24
+    const val quetta: Double = 1E30
+    const val ronna: Double = 1E27
+    const val yotta: Double = 1E24
+    const val zetta: Double = 1E21
+    const val exa: Double = 1E18
+    const val peta: Double = 1E15
+    const val tera: Double = 1E12
+    const val giga: Double = 1E9
+    const val mega: Double = 1E6
+    const val kilo: Double = 1E3
+    const val hecto: Double = 1E2
+    const val deca: Double = 1E1
+    const val deci: Double = 1E-1
+    const val centi: Double = 1E-2
+    const val milli: Double = 1E-3
+    const val micro: Double = 1E-6
+    const val nano: Double = 1E-9
+    const val pico: Double = 1E-12
+    const val femto: Double = 1E-15
+    const val atto: Double = 1E-18
+    const val zepto: Double = 1E-21
+    const val yocto: Double = 1E-24
     @Suppress("SpellCheckingInspection")
-    inline val ronto: Double get() = 1E-27
+    const val ronto: Double = 1E-27
     @Suppress("SpellCheckingInspection")
-    inline val quecto: Double get() = 1E-30
+    const val quecto: Double = 1E-30
 }
 
 inline val Number.quetta: Double get() = this.toDouble() * Prefixes.quetta

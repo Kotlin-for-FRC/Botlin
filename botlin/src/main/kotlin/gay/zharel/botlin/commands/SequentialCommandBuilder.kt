391abd7f5d0a5904797b85cc85cfa7a98a64cf4a
Edan Thomton <edanthomton@gmail.com>
2025-09-23 22:22:11 -0400

    Added a sequential command builder

---
+++ botlin/src/main/kotlin/gay/zharel/botlin/commands/SequentialCommandBuilder.kt
@@ -0,0 +1,60 @@
+package gay.zharel.botlin.commands
+
+import edu.wpi.first.units.measure.Time
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.Subsystem
+import java.util.function.BooleanSupplier
+
+class SequentialCommandBuilder {
+
+    var commands: List<Command> = listOf()
+
+    infix fun runOnce(action: Runnable): SequentialCommandBuilder {
+        commands += Commands.runOnce(action)
+        return this
+    }
+
+    infix fun runCommand(command: Command): SequentialCommandBuilder {
+        commands += command
+        return this
+    }
+
+    infix fun runParallel(commandSet: Set<Command>): SequentialCommandBuilder {
+        commands += Commands.parallel(*commandSet.toTypedArray())
+        return this
+    }
+
+    infix fun runRace(commandSet: Set<Command>): SequentialCommandBuilder {
+        commands += Commands.race(*commandSet.toTypedArray())
+        return this
+    }
+
+    infix fun print(message: String): SequentialCommandBuilder {
+        commands += Commands.print(message)
+        return this
+    }
+
+    infix fun waitSeconds(seconds: Double): SequentialCommandBuilder {
+        commands += Commands.waitSeconds(seconds)
+        return this
+    }
+
+    infix fun waitTime(time: Time): SequentialCommandBuilder {
+        commands += Commands.waitTime(time)
+        return this
+    }
+
+    infix fun waitUntil(condition: BooleanSupplier): SequentialCommandBuilder {
+        commands += Commands.waitUntil(condition)
+        return this
+    }
+
+    infix fun requiring(requirements: Set<Subsystem>): SequentialCommandGroup {
+        val group = SequentialCommandGroup(*commands.toTypedArray())
+        group.addRequirements(*requirements.toTypedArray())
+        return group
+    }
+
+}

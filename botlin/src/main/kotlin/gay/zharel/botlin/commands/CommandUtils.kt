3c3a710b5eae0cba8ab221f3e1d211ab60f064fc
Zach Harel <zach@zharel.me>
2025-09-26 15:20:38 -0400

    feat: add CommandSupplier delegate for lazy command initialization

--- botlin/src/main/kotlin/gay/zharel/botlin/commands/CommandUtils.kt
+++ botlin/src/main/kotlin/gay/zharel/botlin/commands/CommandUtils.kt
@@ -2,6 +2,8 @@ package gay.zharel.botlin.commands
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 /**
  * More convenient access to the CommandScheduler singleton.
@@ -18,5 +20,11 @@ operator fun Command.invoke() = Scheduler.schedule(this)
  */
 infix fun Command.named(name: String): Command = this.withName(name)
 
+/**
+ * Property delegate to initialize a new command instance on every access.
+ */
+class CommandSupplier(private val supplier: () -> Command) : ReadOnlyProperty<Any?, Command> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Command = supplier()
+}
 
 
